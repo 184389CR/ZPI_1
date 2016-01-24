a146e52d894570cac528fb970f0e74f223c22002
184389CR <winiu.winiu@gmail.com>
2016-01-24 16:09:08 +0100

    Dodanie dzielenia

---
+++ main/java/kalkulator/Dzielenie.java
@@ -0,0 +1,18 @@
+package kalkulator;
+
+import base.Dzialanie_base;
+
+public class Odejmowanie implements Dzialanie_base {
+
+	public double wykonaj(double a, double b) {
+		
+		
+		if(a!=0)
+		{
+			return a/b;
+		}
+	}
+	
+
+
+}
